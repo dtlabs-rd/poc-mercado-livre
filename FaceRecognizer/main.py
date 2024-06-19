9d08c2ec62150a034db690f6b2ea01f76642fe29
Lucas Pilla Pimentel <elucaspimentel@gmail.com>
2024-06-19 17:07:33 -0300

    Measure pipeline time

--- FaceRecognizer/main.py
+++ FaceRecognizer/main.py
@@ -1,13 +1,26 @@
 from FaceRecognizer import FaceRecognizer
+import cv2
+import time
         
 # Face recognition pipeline
-pipeline = FaceRecognizer(use_colors=True)
+face_recognition_pipeline = FaceRecognizer(use_colors=True)
 
 # Inference
-faces = pipeline('../data/test_face_recognition.jpg')
+start = time.time()
+img = face_recognition_pipeline.prepare_image('../data/test_face.jpg')
+results = face_recognition_pipeline(img)
+end = time.time()
+
+print(f"Inference took {(end-start)} seconds.")
 
 # Results
-for face in faces:
-    print("Embedding: ", face.vector())
-    print("Direction: ", face.direction())
-    print("Angle: ", face.angle())
+for result in results:
+    print("Embedding shape: ", result.vector().shape)
+    x1, y1, x2, y2 = map(int, result.face.bbox)
+    cv2.rectangle(img, (x1, y1), (x2, y2), color=(0,0,255))
+
+cv2.imshow('Results', img)
+cv2.waitKey(0)
+    
+    
+    
