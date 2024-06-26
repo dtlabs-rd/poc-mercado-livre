44024ce142245142737446b2647dbec08c228216
Lucas Pilla Pimentel <elucaspimentel@gmail.com>
2024-06-26 18:48:20 -0300

    Updated pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -5,6 +5,7 @@ from database.database import Database
 import base64
 import json
 import time
+import asyncio
 
 # Thresholds
 PLATE_DISTANCE_THRESHOLD = 10
@@ -27,6 +28,10 @@ alpr_pipeline = ALPR(
 face_recognition_pipeline = FaceRecognizer()
 
 while True:
+    results = {}
+    
+    time.sleep(1/30)
+    
     # Read frame from streams
     timestamp = time.time()
     _, alpr_frame = alpr_stream.read()
@@ -35,45 +40,40 @@ while True:
     # Run ALPR pipeline
     alpr_result = alpr_pipeline(alpr_frame)
     
-    if len(alpr_result) == 0:
-        continue
-    
-    # Search for plate in database
-    plate, levenshtein_distance = database.query_plate(
-        query_plate=alpr_result[0]['text'], 
-        top_k=1
-    )[0]
-    
-    # Apply plate levenshtein distance threshold
-    if levenshtein_distance > PLATE_DISTANCE_THRESHOLD:
-        continue
+    if len(alpr_result) > 0:
         
-    # Run Face Recognition pipeline
-    face_recognition_result = face_recognition_pipeline(face_recognition_frame)
+        # Search for plate in database
+        plate, levenshtein_distance = database.query_plate(
+            query_plate=alpr_result[0]['text'], 
+            top_k=1
+        )[0]
+        
+        # Apply plate levenshtein distance threshold
+        if levenshtein_distance < PLATE_DISTANCE_THRESHOLD:
+            
+            results["plate_text"] = plate
+            results["plate_bbox"] = alpr_result[0]['detection'].tolist()
+            
+            # Run Face Recognition pipeline
+            face_recognition_result = face_recognition_pipeline(face_recognition_frame)
+            
+            if len(face_recognition_result) > 0:
+                
+                # Search for face in database
+                person, similarity = database.query_face(
+                    query_embedding=face_recognition_result[0].embedding(),
+                    top_k=1
+                )[0]
+            
+                # Apply face similarity threshold
+                if similarity > FACE_SIMILARITY_THRESHOLD:
+                    results["valid"] = plate in person['plates']
+                    results["name"] = person['name']
+                    results["face_bbox"] = face_recognition_result[0].face['bbox'].tolist()
+                    results["face_kpts"] = face_recognition_result[0].face['kps'].tolist()
     
-    if len(face_recognition_result) == 0:
-        continue
-    
-    # Search for face in database
-    person, similarity = database.query_face(
-        query_embedding=face_recognition_result[0].embedding(),
-        top_k=1
-    )[0]
-    
-    # Apply face similarity threshold
-    if similarity < FACE_SIMILARITY_THRESHOLD:
-        continue
-    
-    results = {
-        "valid": plate in person['plates'],
-        "name": person['name'],
-        "plate_text": plate,
-        "plate_bbox": alpr_result[0]['detection'].tolist(),
-        "face_bbox": face_recognition_result[0].face['bbox'].tolist(),
-        "face_kpts": face_recognition_result[0].face['kps'].tolist(),
-        "alpr_frame": base64.b64encode(cv2.imencode('.jpg', alpr_frame)[1].tobytes()).decode('utf-8'),
-        "face_recognition_frame": base64.b64encode(cv2.imencode('.jpg', face_recognition_frame)[1].tobytes()).decode('utf-8'),
-    }
+    results["alpr_frame"] =  base64.b64encode(cv2.imencode('.jpg', alpr_frame)[1].tobytes()).decode('utf-8')
+    results["face_recognition_frame"] = base64.b64encode(cv2.imencode('.jpg', face_recognition_frame)[1].tobytes()).decode('utf-8')
     
     with open(f"./results/{timestamp}.json", "w") as fp:
         json.dump(results, fp)
